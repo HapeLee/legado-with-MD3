f9cda9b341612bcb277d182d043e49701685ac0c
HapeLee <1321903405@qq.com>
2025-10-29 12:41:41 +0800

    图标 #168

--- app/src/main/java/io/legado/app/help/LauncherIconHelp.kt
+++ app/src/main/java/io/legado/app/help/LauncherIconHelp.kt
@@ -2,19 +2,15 @@ package io.legado.app.help
 
 import android.content.ComponentName
 import android.content.pm.PackageManager
-import android.os.Build
-import io.legado.app.R
+import io.legado.app.ui.main.Launcher0
 import io.legado.app.ui.main.Launcher1
 import io.legado.app.ui.main.Launcher2
 import io.legado.app.ui.main.Launcher3
 import io.legado.app.ui.main.Launcher4
 import io.legado.app.ui.main.Launcher5
 import io.legado.app.ui.main.Launcher6
-import io.legado.app.ui.main.Launcher7
 import io.legado.app.ui.main.LauncherW
 import io.legado.app.ui.main.MainActivity
-import io.legado.app.ui.welcome.WelcomeActivity
-import io.legado.app.utils.toastOnUi
 import splitties.init.appCtx
 
 /**
@@ -25,13 +21,13 @@ object LauncherIconHelp {
     private val packageManager: PackageManager = appCtx.packageManager
     private val componentNames = arrayListOf(
         ComponentName(appCtx, LauncherW::class.java.name),
+        ComponentName(appCtx, Launcher0::class.java.name),
         ComponentName(appCtx, Launcher1::class.java.name),
         ComponentName(appCtx, Launcher2::class.java.name),
         ComponentName(appCtx, Launcher3::class.java.name),
         ComponentName(appCtx, Launcher4::class.java.name),
         ComponentName(appCtx, Launcher5::class.java.name),
-        ComponentName(appCtx, Launcher6::class.java.name),
-        ComponentName(appCtx, Launcher7::class.java.name)
+        ComponentName(appCtx, Launcher6::class.java.name)
     )
 
     fun changeIcon(icon: String?) {
