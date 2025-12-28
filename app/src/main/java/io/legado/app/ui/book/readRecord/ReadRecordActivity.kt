8e56c49538fefefad0cbaff358d0e57ebf7f6f35
HapeLee <1321903405@qq.com>
2025-12-29 02:26:21 +0800

    [优化] 为Compose界面实现了View系统的主题系统

--- app/src/main/java/io/legado/app/ui/book/readRecord/ReadRecordActivity.kt
+++ app/src/main/java/io/legado/app/ui/book/readRecord/ReadRecordActivity.kt
@@ -3,7 +3,7 @@ package io.legado.app.ui.book.readRecord
 import android.os.Bundle
 import androidx.compose.runtime.*
 import androidx.lifecycle.lifecycleScope
-import io.legado.app.base.AppTheme
+import io.legado.app.ui.theme.AppTheme
 import io.legado.app.data.appDb
 import io.legado.app.utils.startActivityForBook
 import kotlinx.coroutines.Dispatchers
