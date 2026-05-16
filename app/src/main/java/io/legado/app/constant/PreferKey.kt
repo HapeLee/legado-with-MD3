d6845841b54254f5274fa876169e8a2bc23ecd07
duanhl <duanhl7749@gmail.com>
2026-05-16 18:10:33 +0800

    feature: add translation full content

--- app/src/main/java/io/legado/app/constant/PreferKey.kt
+++ app/src/main/java/io/legado/app/constant/PreferKey.kt
@@ -61,7 +61,6 @@ object PreferKey {
     const val episodeExportFileName = "episodeExportFileName"
     const val recordLog = "recordLog"
     const val processText = "process_text"
-    const val cleanCache = "cleanCache"
     const val saveTabPosition = "saveTabPosition"
     const val fontFolder = "fontFolder"
     const val backupPath = "backupUri"
--- app/src/main/java/io/legado/app/ui/config/ConfigTag.kt
+++ app/src/main/java/io/legado/app/ui/config/ConfigTag.kt
@@ -8,6 +8,5 @@ object ConfigTag {
     const val COVER_CONFIG = "coverConfig"
     const val READ_CONFIG = "readConfig"
     const val DOWNLOAD_CACHE_CONFIG = "downloadCacheConfig"
-    const val TRANSLATION_CONFIG = "translationConfig"
 
 }
