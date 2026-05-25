fbfd08a20c75e6167025904636f9f99ebee8421d
HapeLee <63206378+HapeLee@users.noreply.github.com>
2026-05-25 21:38:47 +0800

    阅读界面自定义图标问题

--- app/src/main/java/io/legado/app/ui/book/read/ReadMenu.kt
+++ app/src/main/java/io/legado/app/ui/book/read/ReadMenu.kt
@@ -637,14 +637,13 @@ class ReadMenu @JvmOverloads constructor(
                 id = "search",
                 iconRes = R.drawable.ic_search,
                 description = context.getString(R.string.search_content),
-                onClick = { runMenuOut{ callBack.openSearchActivity(null) } }
+                onClick = { runMenuOut { callBack.openSearchActivity(null) } }
             ),
             ToolButton(
-                id = "translate",
-                iconRes = R.drawable.ic_translate,
-                description = context.getString(R.string.translate),
-                onClick = { runMenuOut { callBack.onTranslationClick() } },
-                onLongClick = { runMenuOut { callBack.onTranslationLongClick() } }
+                id = "auto_page",
+                iconRes = R.drawable.ic_auto_page,
+                description = context.getString(R.string.auto_next_page),
+                onClick = { runMenuOut { callBack.autoPage() } }
             ),
             ToolButton(
                 id = "catalog",
@@ -706,16 +705,17 @@ class ReadMenu @JvmOverloads constructor(
             ToolButton(
                 id = "replace_badge",
                 iconRes = R.drawable.ic_find_replace,
-                description = context.getString(R.string.replace_purify),
+                description = context.getString(R.string.replace_purify_badge),
                 onLongClick = { runMenuOut { callBack.openReplaceRule() } },
                 onCheck = { runMenuOut { callBack.changeReplaceRuleState() } },
                 onClick = {  }
             ),
             ToolButton(
-                id = "auto_page",
-                iconRes = R.drawable.ic_auto_page,
-                description = context.getString(R.string.auto_next_page),
-                onClick = { runMenuOut { callBack.autoPage() } }
+                id = "translate",
+                iconRes = R.drawable.ic_translate,
+                description = context.getString(R.string.translate),
+                onClick = { runMenuOut { callBack.onTranslationClick() } },
+                onLongClick = { runMenuOut { callBack.onTranslationLongClick() } }
             )
         )
     }
