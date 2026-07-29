078b11efb53646aedea77c15d623b5d0a4fd3304
HapeLee <63206378+HapeLee@users.noreply.github.com>
2026-07-29 22:02:29 +0800

    feat: 重写书源编辑界面

--- app/src/main/java/io/legado/app/di/appModule.kt
+++ app/src/main/java/io/legado/app/di/appModule.kt
@@ -221,6 +221,7 @@ import io.legado.app.ui.book.readaloud.player.ReadAloudPlayerCoordinator
 import io.legado.app.ui.book.readaloud.player.ReadAloudPlayerViewModel
 import io.legado.app.ui.book.search.SearchViewModel
 import io.legado.app.ui.book.searchContent.SearchContentViewModel
+import io.legado.app.ui.book.source.edit.BookSourceEditViewModel
 import io.legado.app.ui.book.source.manage.BookSourceViewModel
 import io.legado.app.ui.book.toc.TocViewModel
 import io.legado.app.ui.book.toc.rule.TxtTocRuleViewModel
@@ -449,6 +450,7 @@ val appModule = module {
     viewModelOf(::DictViewModel)
     viewModelOf(::RssSourceViewModel)
     viewModelOf(::BookSourceViewModel)
+    viewModelOf(::BookSourceEditViewModel)
     viewModelOf(::RssSortViewModel)
     viewModelOf(::RssArticlesViewModel)
     viewModelOf(::ReadRssViewModel)
